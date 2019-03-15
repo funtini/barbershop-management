7b4e585fcda384c716254ed68ba48a8213936b3a
jpgomes <jpgomes89@gmail.com>
2019-03-15 19:42:24 +0000

    Integrate redux part 1

--- frontend/bsm-react/src/index.js
+++ frontend/bsm-react/src/index.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose } from 'redux'
 import * as serviceWorker from './serviceWorker';
 import './i18n';
 
+// middleware
+import thunk from 'redux-thunk';
+
+// components
+import App from './App';
+// styles
+import './index.css';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+    applyMiddleware(thunk)
+));
+
 ReactDOM.render(
-        <App />,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
 
